d3098bf283e8161fcccb3644c1d877c3c470bc4d
AvinashDev <55042628+Avinash-dev-code@users.noreply.github.com>
2020-04-16 23:40:30 +0530

    added static keyword program

---
+++ StaticKeyword.java
@@ -0,0 +1,22 @@
+
+public class StaticKeyword {
+
+     static 
+
+    {
+        System.out.println("first static statement");
+    }
+    static
+    {
+        System.out.println("Second static statement");
+    }
+
+
+
+
+
+    public static void main(final String[] args) {
+        
+        System.out.println("This is the main static area.");
+    }
+}
