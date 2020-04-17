8ce5ade21d151fbf013e1d4384d3d82c69851709
AvinashDev <55042628+Avinash-dev-code@users.noreply.github.com>
2020-04-17 10:55:15 +0530

    increment and decrementoperator

---
+++ Basics.java
@@ -0,0 +1,17 @@
+
+public class Basics {
+
+
+
+    public static void main(String[] args) {
+        int age=11;
+        int newAge=age++;
+        int newAge1=++age;
+        int newAge2=age--;
+        int newAge3=--age;
+        System.out.println(newAge);
+        System.out.println(newAge1);
+        System.out.println(newAge2+" ");
+        System.out.println(newAge3+" ");
+    }
+}
